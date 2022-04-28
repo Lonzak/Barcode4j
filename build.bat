b9fe5f1aef2b4e1fe126fbf923a4436d731a75d0
Tobias <tvtreeck@nepatec.de>
2022-04-28 11:53:11 +0200

    Removed old package.htmls

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@
 ECHO === build Barcode4J Utils ===
 ECHO mvn -U -e clean install
 set MAVEN_OPTS=-Dfile.encoding=UTF-8 -Xmx4192m
-call mvn -U -e clean install
+call mvn -U -e clean install -Pdistribution
 pause
